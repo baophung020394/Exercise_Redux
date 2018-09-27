3ad3bb840cc8401661ce236bd56168e4b075bfe1
Bao <bao.future94@gmail.com>
2018-09-28 00:10:41 +0700

    xong them,xoa,sua,xem firebase

--- 109_redux/src/App.js
+++ 109_redux/src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {firebaseConnect} from './firebaseConnect';
+import * as firebase from 'firebase';
 class App extends Component {
+
+  pushData = () => {
+    var connectData = firebase.database().ref('dataForNote');
+    connectData.push({
+      id: "4",
+      titleNote : "Title demo",
+      contentNote: "content demo"
+    })
+  }
+
+
+  deleteData = (id) => {
+    var connectData = firebase.database().ref('dataForNote');
+    connectData.child(id).remove();
+    console.log('xoa rồi má ơi');
+  }
+
   render() {
     console.log(firebaseConnect);
     return (
@@ -11,9 +29,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => this.pushData()}>Click me</button>
+        <button onClick={() => this.deleteData("-LNQslYJGUxhUUEKss6W")}>Xóa me</button>
       </div>
     );
   }
